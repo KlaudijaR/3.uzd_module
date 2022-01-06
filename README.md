859b910a14d549f9d3ad9fbec61cda05a31d9db9
Klaudija Roke <97155837+KlaudijaR@users.noreply.github.com>
2022-01-06 07:39:14 +0000

    iesaku 8.uzdevumu

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ eksportēt izmaiņas uz GitHub
 pārliecināties, ka tās ir redzamas GitHub
 7.uzdevums       
 Uzrakstīt funkciju, kas dotajam nedēļas dienas numuram atgriež dienas nosaukumu vārdiem.
+8.uzdevums
+Uzrakstīt funkciju, kas aprēķina romba laukumu, kam zināmas visu četru punktu koordinātes.
+S=d1d2/2, kur d1 un d2 ir diagonāļu garumi
--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 import nedela
 diena = int(input("Ievadi dienu no 1 līdz 7: "))
 nedela.nedela(diena)
+
+"""#Uzrakstīt funkciju, kas aprēķina romba laukumu, kam zināmas visu četru punktu koordinātes.
+S=d1d2/2, kur d1 un d2 ir diagonāļu garumi"""
+d1 = int(input("Ievadi 1. diagonāles garumu: "))
+d2 = int(input("Ievadi 2. diagonāles garumu: "))"""
