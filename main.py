ca8026dfd77747bde2548402a29adefa44b7e6e6
Klaudija Roke <97155837+KlaudijaR@users.noreply.github.com>
2022-01-06 07:39:44 +0000

    Izlaboju vienu kludu 8 uzd

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ diena = int(input("Ievadi dienu no 1 līdz 7: "))
 nedela.nedela(diena)
 
 """#Uzrakstīt funkciju, kas aprēķina romba laukumu, kam zināmas visu četru punktu koordinātes.
-S=d1d2/2, kur d1 un d2 ir diagonāļu garumi"""
+S=d1d2/2, kur d1 un d2 ir diagonāļu garumi
 d1 = int(input("Ievadi 1. diagonāles garumu: "))
 d2 = int(input("Ievadi 2. diagonāles garumu: "))"""
